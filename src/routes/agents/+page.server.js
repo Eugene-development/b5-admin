ba0d4d9e8f4574624321a1e7b08d80e84c8bd524
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-08-17 12:40:58 +0300

    c

---
+++ src/routes/agents/+page.server.js
@@ -0,0 +1,40 @@
+// import { gql, request } from 'graphql-request';
+
+// export const load = async () => {
+// 	const query = gql`
+// 		query {
+// 			users {
+// 				id
+// 				city
+// 			}
+// 		}
+// 	`;
+
+// 	const variables = {};
+
+// 	const data = await request(process.env.B5_API_URL, query, variables);
+// 	console.log(data);
+
+// 	return {
+// 		agents: data.users
+// 	};
+// };
+
+import { gql, request } from 'graphql-request';
+
+export const load = async () => {
+	const query = gql`
+		{
+			users {
+				id
+				city
+			}
+		}
+	`;
+	const data = await request(import.meta.env.VITE_B5_API_URL, query);
+	console.log(data);
+
+	return {
+		agents: data.users
+	};
+};
---
+++ src/routes/agents/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	let { data } = $props();
+	console.log(data);
+</script>
+
+<div>
+	<h1>Agents</h1>
+	{#each data.agents as agent}
+		<p>{agent.id}</p>
+	{/each}
+</div>
