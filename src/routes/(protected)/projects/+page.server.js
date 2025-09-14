8925137d300595b57d6919c2dc600b9116048765
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-09-14 19:23:05 +0300

    c

--- src/routes/(protected)/projects/+page.server.js
+++
@@ -1,9 +0,0 @@
-// Server-side data loading removed - all data will be loaded client-side
-// after authentication is initialized to avoid localStorage access on server
-export const load = async ({ fetch, url }) => {
-	// Return empty initial state - data will be loaded client-side
-	return {
-		projects: [],
-		error: null
-	};
-};
