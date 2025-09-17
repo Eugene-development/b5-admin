3729a3af48d31758d3dbcca29e9ef6f528a5552a
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-09-17 12:54:47 +0300

    c

--- src/lib/components/ActionButtons.svelte
+++ src/lib/components/ActionButtons.svelte
@@ -115,8 +115,8 @@
 </script>
 
 {#if mobile}
-	<!-- Mobile Layout - Larger touch targets, full width buttons -->
-	<div class="flex w-full flex-col space-y-2">
+	<!-- Mobile Layout - Larger touch targets, actions in one row -->
+	<div class="flex w-full flex-row justify-end space-x-2">
 		{#if projectMode && onView}
 			<!-- View Button -->
 			<button
@@ -131,7 +131,7 @@
 			>
 				{#if isLoading}
 					<svg
-						class="mr-2 h-4 w-4 animate-spin"
+						class="h-4 w-4 animate-spin"
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
 						viewBox="0 0 24 24"
@@ -147,7 +147,7 @@
 					</svg>
 				{:else}
 					<svg
-						class="mr-2 h-5 w-5"
+						class="h-5 w-5"
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
 						viewBox="0 0 24 24"
@@ -167,7 +167,6 @@
 							d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
 						/>
 					</svg>
-					Просмотреть
 				{/if}
 			</button>
 		{/if}
