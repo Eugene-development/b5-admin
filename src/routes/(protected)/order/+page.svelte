4096d8bf20ab8218cc1c45942ba0ff4bfe013c9a
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-10-11 12:56:58 +0300

    с

--- src/routes/(protected)/order/+page.svelte
+++ src/routes/(protected)/order/+page.svelte
@@ -246,7 +246,7 @@
 				<!-- Add Order Button -->
 				<button
 					type="button"
-					class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+					class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 				>
 					<svg
 						class="-ml-0.5 mr-1.5 h-5 w-5"
